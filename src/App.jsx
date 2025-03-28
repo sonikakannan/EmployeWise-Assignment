65cf9b80078992f043d97466ef513686d4778e50
sonikakannan <sonikakannan66@gmail.com>
2025-03-28 11:35:44 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
-import UsersPage from './pages/userPage'
+import UsersPage from './pages/UserPage'
 
 
 const App = () => {
